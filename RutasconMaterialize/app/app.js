76ef05cc14cd0373ef1e324f2c5161f760f70832
QuetzalB1 <quetzalcoatlbarron82@aragon.unam.mx>
2025-05-06 19:32:36 -0600

    Rutas con CSS de Materialize

---
+++ RutasconMaterialize/app/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require("../routes/routes.js");
+const app = express();
+const path = require('path');
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../css')));
+
+app.use("/", routes);
+
+app.listen(port, () => {
+    console.log(`http://127.0.0.1:${port}`);
+});
